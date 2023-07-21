98a65e1176950ad1eb87ed6aaf29802c935923c7
Matt Ortner <64284137+MattOrtner@users.noreply.github.com>
2023-07-21 09:42:40 -0400

    imgData reconfig for comp. (#80)

--- src/components/organisms/LeftProductPageContainer.jsx
+++ src/components/organisms/LeftProductPageContainer.jsx
@@ -1,32 +1,20 @@
-import React from "react";
 import styled from "styled-components";
 import "react-image-gallery/styles/css/image-gallery.css";
 
 import ProductReviewsContainer from "../molecules/ProductReviewsContainer";
 import ImageGallery from "react-image-gallery";
-const images = [
-  {
-    original: "https://picsum.photos/id/1018/1000/600/",
-    thumbnail: "https://picsum.photos/id/1018/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1015/1000/600/",
-    thumbnail: "https://picsum.photos/id/1015/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1019/1000/600/",
-    thumbnail: "https://picsum.photos/id/1019/250/150/",
-  },
-];
-const LeftProductPageContainer = ({ productName }) => {
+
+const LeftProductPageContainer = ({ productName, images }) => {
   return (
     <LeftContainer>
-      <ImageGallery
-        items={images}
-        thumbnailPosition="left"
-        useBrowserFullscreen={false}
-        showPlayButton={false}
-      />
+      {images && (
+        <ImageGallery
+          items={images}
+          thumbnailPosition="left"
+          useBrowserFullscreen={false}
+          showPlayButton={false}
+        />
+      )}
       <ProductReviewsContainer productName={productName} />
     </LeftContainer>
   );
